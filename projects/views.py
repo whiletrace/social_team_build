8341748e797714bdbb4d4685460aeefbf224255c
Trace Harris <traceh@whiletrace.com>
2021-04-16 00:58:52 -0700

    Unittest for post method to URL /projects/create_project

--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,7 @@ class CreateProject(LoginRequiredMixin, CreateView):
     model = UserProject
 
     fields = ['title', 'description', 'timeline', 'requirements']
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
--- testing/test_projects.py
+++ testing/test_projects.py
@@ -1,5 +1,7 @@
 import pytest
 
+from projects.models import UserProject
+
 
 # test models
 
@@ -14,5 +16,19 @@ def test_project_model(make_test_project):
 @pytest.mark.django_db
 def test_create_project(client):
     response = client.get('/projects/create_project/')
-
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_post(client, make_test_user):
+    user = make_test_user
+    client.force_login(user)
+    response = client.post('/projects/create_project/', {
+        'created_by':make_test_user,
+        'title':'bigguy',
+        'description':'this is bio and what of it',
+        'timeline':'whenever',
+        'requirements':'so many requirements'
+        })
+
+    assert UserProject.objects.last().title == 'bigguy'
