c64295dabcff2f3bae2d9b2780b0ae07f6dd06db
Trace Harris <traceh@whiletrace.com>
2021-05-09 02:09:08 -0700

    hotfix media file

--- social_team_build/urls.py
+++ social_team_build/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
                   path('projects/',
                        include('projects.urls', namespace='projects')),
 
-                  ] + static(settings.STATIC_URL,
-                             document_root=settings.STATIC_ROOT)
+                  ] + static(settings.MEDIA_URL,
+                             document_root=settings.MEDIA_ROOT)
