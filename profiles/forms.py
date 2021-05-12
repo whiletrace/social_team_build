5ecef6ee4ad5fe74c41fa1a48f171040ee9d9824
Trace Harris <traceh@whiletrace.com>
2021-05-11 23:16:12 -0700

    added helptext to skills field ProfileForm to make a bit better user experience also fixed link to edit profile

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,12 +7,16 @@ from .models import UserProfile
 # This is where profile forms will live
 
 class ProfileForm(ModelForm):
-    skills = forms.CharField(strip=True, required=False)
+    skills = forms.CharField(strip=True, required=False,
+                             help_text='multiple skills should '
+                                       'be separated by comma')
+
 
     class Meta:
         model = UserProfile
         fields = ['username', 'bio', 'avatar']
         exclude = ['created_by']
+
 
     def __str__(self):
         return str.split(',')
