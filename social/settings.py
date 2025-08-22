e407c837463176d0b6b6dafd4fc62ab0e052b10e
whiletrace <traceh38@gmail.com>
2025-08-21 21:41:52 -0700

    change database production settings to trouble shoot connection issues

--- social/settings.py
+++ social/settings.py
@@ -95,20 +95,13 @@ os.environ.setdefault("PGPASSWORD", "")
 os.environ.setdefault("PGHOST", "localhost")
 os.environ.setdefault("PGPORT", "5432")
 
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default':
-                dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
+iDATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 
 
