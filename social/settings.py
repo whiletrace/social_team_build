337ac80ef02996c7260fd209862640aff2fb7458
whiletrace <traceh38@gmail.com>
2025-08-21 17:42:39 -0700

    debugging deployment

--- social/settings.py
+++ social/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+qu
 
 ALLOWED_HOSTS = ["*"]
 
